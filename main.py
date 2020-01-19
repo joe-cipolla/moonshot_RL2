0d28f904965c62be09080efb1802fd6f40ecde37
Joe Cipolla <jlcipolla@gmail.com>
2020-01-19 16:44:53 -0600

    optimize_network agent func

--- main.py
+++ main.py
@@ -546,3 +546,44 @@ assert(np.allclose(delta_vec, answer_delta_vec))
 print("Passed the asserts! (Note: These are however limited in scope, additional testing is encouraged.)")
 
 
+def optimize_network(experiences, discount, optimizer, network, current_q, tau):
+    """
+    Args:
+        experiences (Numpy array): The batch of experiences including the states, actions, 
+                                   rewards, terminals, and next_states.
+        discount (float): The discount factor.
+        network (ActionValueNetwork): The latest state of the network that is getting replay updates.
+        current_q (ActionValueNetwork): The fixed network used for computing the targets, 
+                                        and particularly, the action-values at the next-states.
+    """
+    
+    # Get states, action, rewards, terminals, and next_states from experiences
+    states, actions, rewards, terminals, next_states = map(list, zip(*experiences))
+    states = np.concatenate(states)
+    next_states = np.concatenate(next_states)
+    rewards = np.array(rewards)
+    terminals = np.array(terminals)
+    batch_size = states.shape[0]
+
+    # Compute TD error using the get_td_error function
+    # Note that q_vec is a 1D array of shape (batch_size)
+    delta_vec = get_td_error(states, next_states, actions, rewards, discount, terminals, network, current_q, tau)
+
+    # Batch Indices is an array from 0 to the batch_size - 1. 
+    batch_indices = np.arange(batch_size)
+
+    # Make a td error matrix of shape (batch_size, num_actions)
+    # delta_mat has non-zero value only for actions taken
+    delta_mat = np.zeros((batch_size, network.num_actions))
+    delta_mat[batch_indices, actions] = delta_vec
+
+    # Pass delta_mat to compute the TD errors times the gradients of the network's weights from back-propagation    
+    td_update = network.get_TD_update(states, delta_mat)
+    
+    # Pass network.get_weights and the td_update to the optimizer to get updated weights
+    weights = optimizer.update_weights(network.get_weights(), td_update)
+    
+    network.set_weights(weights)
+
+
+
